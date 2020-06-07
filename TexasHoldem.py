d300cd15fc271267fccd43fcbac9fa3c3a4465f0
_BZB_ <parksiyeon1020@gmail.com>
2020-06-07 15:12:16 +0900

    텍사스 홀덤 기호작업 시작

---
+++ TexasHoldem.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import font
+from winsound import *
+import player
+import card
+import random
+
+class TexasHoldem:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Texas Holdem")
+        self.window.geometry("800x600")
+        self.window.configure(bg="green")
+        self.fontstyle = font.Font(self.window, size=24, weight='bold', family='Consolas')
+        self.fontstyle2 = font.Font(self.window, size=16, weight='bold', family='Consolas')
+        self.setupButton()
+        self.setupLabel()
+
+        self.window.mainloop()
+
+    def setupButton(self):
+        self.check = Button(self.window, text="Check", width=6, height=1, font=self.fontstyle2, command=self.pressedCheck)
+        self.check.place(x=50, y=500)
+        self.Bx1 = Button(self.window, text="Bet x1", width=6, height=1, font=self.fontstyle2, command=self.pressedBet1)
+        self.Bx1.place(x=150, y=500)
+        self.Bx2 = Button(self.window, text="Bet x2", width=6, height=1, font=self.fontstyle2, command=self.pressedBet2)
+        self.Bx2.place(x=250, y=500)
+        self.Deal = Button(self.window, text="Deal", width=6, height=1, font=self.fontstyle2, command=self.pressedDeal)
+        self.Deal.place(x=600, y=500)
+        self.Again = Button(self.window, text="Again", width=6, height=1, font=self.fontstyle2,
+                            command=self.pressedAgain)
+        self.Again.place(x=700, y=500)
+
+        self.Deal['state'] = 'disabled'
+        self.Deal['bg'] = 'gray'
+        self.Again['state'] = 'disabled'
+        self.Again['bg'] = 'gray'
+
+    def setupLabel(self):
+        self.LbetMoney = Label(text="$0", width=4, height=1, font=self.fontstyle, bg="green", fg="cyan")
+        self.LbetMoney.place(x=200, y=450)
+        self.LplayerMoney = Label(text="You have $1000", width=15, height=1, font=self.fontstyle, bg="green", fg="cyan")
+        self.LplayerMoney.place(x=500, y=450)
+        self.LplayerPts = Label(text="", width=2, height=1, font=self.fontstyle2, bg="green", fg="white")
+        self.LplayerPts.place(x=300, y=300)
+        self.LdealerPts = Label(text="", width=2, height=1, font=self.fontstyle2, bg="green", fg="white")
+        self.LdealerPts.place(x=300, y=100)
+        self.Lstatus = Label(text="", width=15, height=1, font=self.fontstyle, bg="green", fg="white")
+        self.Lstatus.place(x=500, y=300)
---
+++ player.py
@@ -0,0 +1,9 @@
+from card import *
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+        self.N = 0
+
