9535e439d8fafdceff779ef35f2172a0b81ae193
daydreamwarrior <32808663+daydreamwarrior@users.noreply.github.com>
2020-06-18 17:51:56 +0900

    메이드+족보 출력, 어게인시 라벨 초기화, 메이드된 카드 넘버 라벨 색 변환

--- 2020-1 스크립트언어 텀프로젝트/DorijitGo.py
+++ 2020-1 스크립트언어 텀프로젝트/DorijitGo.py
@@ -54,6 +54,7 @@ class DorijitGo:
         self.LScoresPlayer1=[0 for _ in range(5)]
         self.LScoresPlayer2 = [0 for _ in range(5)]
         self.LScoresPlayer3 = [0 for _ in range(5)]
+        self.LScoresDealer=[0 for _ in range(5)]
 
         self.deckN = 0
         #self.cardsphotoimage = [0 for _ in range(40)] # 20에서 40으로 교체
@@ -103,17 +104,24 @@ class DorijitGo:
                                 fg="orange")
         self.LUserMoney.place(x=605, y=450)
 
-        self.Lplayer1State = Label(text="여기", width=12, height=1, font=self.fontstyle2, bg="green", fg="white")
+        self.Lplayer1State = Label(text="", width=8, height=1, font=self.fontstyle2, bg="green", fg="white")
         self.Lplayer1State.place(x=100, y=400)
-        self.Lplayer2State = Label(text="여기", width=12, height=1, font=self.fontstyle2, bg="green", fg="white")
+        self.Lplayer2State = Label(text="", width=8, height=1, font=self.fontstyle2, bg="green", fg="white")
         self.Lplayer2State.place(x=300, y=400)
-        self.Lplayer3State = Label(text="여기", width=12, height=1, font=self.fontstyle2, bg="green", fg="white")
+        self.Lplayer3State = Label(text="", width=8, height=1, font=self.fontstyle2, bg="green", fg="white")
         self.Lplayer3State.place(x=500, y=400)
+        self.LdealerState = Label(text="", width=8, height=1, font=self.fontstyle2, bg="green", fg="white")
+        self.LdealerState.place(x=500, y=100)
 
-        #self.LdealerState = Label(text="", width=12, height=1, font=self.fontstyle2, bg="green", fg="white")
-        #self.LdealerState.place(x=300, y=100)
-        #self.Lstatus = Label(text="", width=12, height=1, font=self.fontstyle, bg="green", fg="red")
-        #self.Lstatus.place(x=550, y=350)
+        #메이드 뒤에 붙는 애들
+        self.Lplayer1StateJB=Label(text="",width=8,height=1,font=self.fontstyle2,bg="green", fg="white")
+        self.Lplayer1StateJB.place(x=170,y=400)
+        self.Lplayer2StateJB = Label(text="", width=8, height=1,font=self.fontstyle2, bg="green", fg="white")
+        self.Lplayer2StateJB.place(x=370, y=400)
+        self.Lplayer3StateJB = Label(text="", width=8, height=1,font=self.fontstyle2, bg="green", fg="white")
+        self.Lplayer3StateJB.place(x=570, y=400)
+        self.LdealerStateJB = Label(text="", width=8, height=1,font=self.fontstyle2, bg="green", fg="white")
+        self.LdealerStateJB.place(x=570, y=100)
 
         #플레이어 각 화투패 점수 나오는 라 벨,,,,
         for i in range(5):
@@ -125,6 +133,9 @@ class DorijitGo:
 
             self.LScoresPlayer3[i] = Label(text="", width=2, height=1, font=self.fontstyle2, bg="green", fg="white")
             self.LScoresPlayer3[i].place(x=500 + i * 20, y=250)
+
+            self.LScoresDealer[i] = Label(text="", width=2, height=1, font=self.fontstyle2, bg="green", fg="white")
+            self.LScoresDealer[i].place(x=500 + i * 20, y=50)
 
 
     def PressedUser1_5milB(self):
@@ -384,7 +395,7 @@ class DorijitGo:
             p = PhotoImage(file='GodoriCards/' + self.dealer.cards[i][1])
             self.LcardsDealer[i].configure(image=p)  # 이미지 레퍼런스 변경
             self.LcardsDealer[i].image = p  # 파이썬은 라벨 이미지 레퍼런스를 갖고 있어야 이미지가 보임
-            #self.LdealerState.configure(text=str(self.dealer.value()))
+            self.LScoresDealer[i].configure(text=self.dealer.cards[i][0])
         self.CheckWinner()
         #딜러점수 라벨에 보여야함여
 
@@ -399,10 +410,15 @@ class DorijitGo:
         self.betMoney3=0
 
         self.StartButtonState()
-        #self.LplayerState.configure(text="")
-        #self.LdealerState.configure(text="")
-        #self.Lstatus.configure(text="")
-        #self.Lstatus.configure(text="")
+        self.Lplayer1State.configure(text="")
+        self.Lplayer2State.configure(text="")
+        self.Lplayer3State.configure(text="")
+        self.LdealerState.configure(text="")
+        self.Lplayer1StateJB.configure(text="")
+        self.Lplayer2StateJB.configure(text="")
+        self.Lplayer3StateJB.configure(text="")
+        self.LdealerStateJB.configure(text="")
+
         self.LUser1_betMoney.configure(text="0만")
         self.LUser2_betMoney.configure(text="0만")
         self.LUser3_betMoney.configure(text="0만")
@@ -420,6 +436,7 @@ class DorijitGo:
             self.LScoresPlayer1[i].configure(text="")
             self.LScoresPlayer2[i].configure(text="")
             self.LScoresPlayer3[i].configure(text="")
+            self.LScoresDealer[i].configure(text="")
 
         self.LcardsPlayer1.clear()
         self.LcardsPlayer2.clear()
@@ -441,6 +458,7 @@ class DorijitGo:
         self.Player1Check()
         self.Player2Check()
         self.Player3Check()
+        self.DealerCheck()
         # if self.pCheck > self.dCheck:
         #     #PlaySound('Resources/sounds/win.wav', SND_FILENAME)
         #     self.Lstatus.configure(text="Win")
@@ -490,12 +508,14 @@ class DorijitGo:
                                 for j in range(5):
                                     if j != idx and j != idx2 and j != idx3:
                                         self.toJokbo1.append(p1val[j])
-                                print(self.toJokbo1)
+                                self.LScoresPlayer1[idx].configure(fg="yellow")
+                                self.LScoresPlayer1[idx2].configure(fg="yellow")
+                                self.LScoresPlayer1[idx3].configure(fg="yellow")
                                 self.Lplayer1State.configure(text=str(self.cardtable[i][3]))
-                                return i
+                                self.JokboCheck(self.toJokbo1,1)
+                                return
 
         self.Lplayer1State.configure(text="노메이드")
-        return -1 #노메이드
 
     def Player2Check(self):
         p2val = [0 for _ in range(5)]
@@ -522,12 +542,14 @@ class DorijitGo:
                                 for j in range(5):
                                     if j != idx and j != idx2 and j != idx3:
                                         self.toJokbo2.append(p2val[j])
-                                print(self.toJokbo2)
+                                self.LScoresPlayer2[idx].configure(fg="yellow")
+                                self.LScoresPlayer2[idx2].configure(fg="yellow")
+                                self.LScoresPlayer2[idx3].configure(fg="yellow")
                                 self.Lplayer2State.configure(text=str(self.cardtable[i][3]))
-                                return i
+                                self.JokboCheck(self.toJokbo2,2)
+                                return
 
         self.Lplayer2State.configure(text="노메이드")
-        return -1
 
     def Player3Check(self):
         p3val = [0 for _ in range(5)]
@@ -554,25 +576,119 @@ class DorijitGo:
                                 for j in range(5):
                                     if j != idx and j != idx2 and j != idx3:
                                         self.toJokbo3.append(p3val[j])
-                                print(self.toJokbo3)
+                                self.LScoresPlayer3[idx].configure(fg="yellow")
+                                self.LScoresPlayer3[idx2].configure(fg="yellow")
+                                self.LScoresPlayer3[idx3].configure(fg="yellow")
                                 self.Lplayer3State.configure(text=str(self.cardtable[i][3]))
-                                return i
+                                self.JokboCheck(self.toJokbo3,3)
+                                return
 
         self.Lplayer3State.configure(text="노메이드")
-        return -1
                         
     def DealerCheck(self):
-        pass
+        dval = [0 for _ in range(5)]
+        self.toJokboD = []  # 메이드 3개 빼놓을 리스트
+        idx = 0
+        idx2 = 0
+        idx3 = 0
+        key = 0
 
-    def JokboCheck(self,lst):
-        #         # 3장을 메이르도 완성한 뒤 나머지 2장으로 족보 비교
-        #
+        for i in range(5):
+            dval[i] = self.dealer.cards[i][0]  # 점수 각각 저장
+
+        for i in range(21):
+            if self.cardtable[i][0] in dval[:]:  # 찾아서 있으면
+                idx = dval.index(self.cardtable[i][0])  # 체크할 인덱스를 p1val 인덱스로 바꿈
+                key = i  # 족보 인덱스
+                if self.cardtable[key][1] in dval[:]:  # key 족보 두번째 요소 찾기
+                    if dval.index(self.cardtable[key][1]) != idx:
+                        idx2 = dval.index(self.cardtable[i][1])
+                        if self.cardtable[key][2] in dval[:]:
+                            if dval.index(self.cardtable[key][2]) != idx2 and dval.index(
+                                    self.cardtable[key][2]) != idx:
+                                idx3 = dval.index(self.cardtable[i][2])
+                                for j in range(5):
+                                    if j != idx and j != idx2 and j != idx3:
+                                        self.toJokboD.append(dval[j])
+                                self.LdealerState.configure(text=str(self.cardtable[i][3]))
+                                self.LScoresDealer[idx].configure(fg="yellow")
+                                self.LScoresDealer[idx2].configure(fg="yellow")
+                                self.LScoresDealer[idx3].configure(fg="yellow")
+                                self.JokboCheck(self.toJokboD,4)
+                                return
+
+        self.LdealerState.configure(text="노메이드")
+
+    def JokboCheck(self,lst,num):
+
         #         # 1순위 - 2장 self.value가 3,8 >> 38광땡
         #         # 2순위 - self.value가 1,3 또는 1,8 >> 광땡
         #         # 3순위 - self.value가 같을 때 >> 땡 (월이 높을 수록 높은 족보)
         #         # 4순위 - 1,2,3 순위 해당x 두 패의 숫자 합이 1~8 >> 끗
-        #         # 5순위 - 2,8월 또는 3,7월 끗 수가 0 >> 가장 낮음
-        print("넘어온 리스트:",lst)
+        #         # 5순위 - 2,8월 또는 3,7월 끗 수가 0 >> 가장낮음>> 망통
+
+
+        lst.sort()
+        if lst==[3,8]:
+            if num==1:
+                self.Lplayer1StateJB.configure(text="38광땡")
+            elif num==2:
+                self.Lplayer2StateJB.configure(text="38광땡")
+            elif num==3:
+                self.Lplayer3StateJB.configure(text="38광땡")
+            elif num==4:
+                self.LdealerStateJB.configure(text="38광땡")
+                
+        elif lst==[1,3] or lst==[1,8]:
+            if num==1:
+                self.Lplayer1StateJB.configure(text=str(lst[0])+str(lst[1])+"광땡")
+            elif num==2:
+                self.Lplayer2StateJB.configure(text=str(lst[0])+str(lst[1])+"광땡")
+            elif num==3:
+                self.Lplayer3StateJB.configure(text=str(lst[0])+str(lst[1])+"광땡")
+            elif num==4:
+                self.LdealerStateJB.configure(text=str(lst[0])+str(lst[1])+"광땡")
+                
+        elif lst[0]==lst[1]:
+            if num==1:
+                self.Lplayer1StateJB.configure(text=str(lst[0])+"땡")
+            elif num==2:
+                self.Lplayer2StateJB.configure(text=str(lst[0])+"땡")
+            elif num==3:
+                self.Lplayer3StateJB.configure(text=str(lst[0])+"땡")
+            elif num==4:
+                self.LdealerStateJB.configure(text=str(lst[0])+"땡")
+
+        elif 1<=lst[0]+lst[1] and lst[0]+lst[1]<=8:
+            if num==1:
+                self.Lplayer1StateJB.configure(text=str(lst[0]+lst[1])+"끗")
+            elif num==2:
+                self.Lplayer2StateJB.configure(text=str(lst[0]+lst[1])+"끗")
+            elif num==3:
+                self.Lplayer3StateJB.configure(text=str(lst[0]+lst[1])+"끗")
+            elif num==4:
+                self.LdealerStateJB.configure(text=str(lst[0]+lst[1])+"끗")
+
+        elif (lst[0]==2 and lst[1]==8)or (lst[0]==3 and lst[1]==7):
+            if num==1:
+                self.Lplayer1StateJB.configure(text="망통")
+            elif num==2:
+                self.Lplayer2StateJB.configure(text="망통")
+            elif num==3:
+                self.Lplayer3StateJB.configure(text="망통")
+            elif num==4:
+                self.LdealerStateJB.configure(text="망통")
+                
+        #해당없으면 출력 안함
+        # else:
+        #     if num==1:
+        #         self.Lplayer1StateJB.configure(text="해당없음")
+        #     elif num==2:
+        #         self.Lplayer2StateJB.configure(text="뭐")
+        #     elif num==3:
+        #         self.Lplayer3StateJB.configure(text="뭐")
+        #     elif num==4:
+        #         self.LdealerStateJB.configure(text="뭐")
 
     def inputCardList(self):
         n = 0
