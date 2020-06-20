b7034d3bebacf01d3cedd03d42b150e3b82543fb
_BZB_ <parksiyeon1020@gmail.com>
2020-06-20 12:57:27 +0900

    챗봇 (구동 x

---
+++ 2020-1 스크립트언어 텀프로젝트/ChatBotParsing.py
@@ -0,0 +1,130 @@
+import telegram
+from bs4 import *
+import requests
+import time
+import telegramBot
+from telepot.loop import MessageLoop
+
+class setData:
+    def __init__(self):
+        self.token = '1157639195:AAElOOwbzdQAhliqYjeAqieNA3F7ImUXK9w'
+        self.bot = telegram.Bot(self.token)
+        id = 1132666012
+
+        self.DataList = [[0] * 25 for _ in range(9)]
+        self.DataList.append([])
+        self.GetxmlFile()
+        self.printData()
+
+        update = self.bot.getUpdates()
+
+        self.bot.getMe() # 텔레그램 정보 확인용
+        self.startChat()
+
+
+
+
+
+    def GetxmlFile(self):
+        import http.client
+        from xml.dom.minidom import parse, parseString
+        conn = http.client.HTTPConnection("openapi.seoul.go.kr:8088")
+        conn.request("GET", "/624a754e616d696e35326b42565763/xml/ListAirQualityByDistrictService/1/25")
+        req = conn.getresponse()
+
+        if req.status == 200:
+            SeoulAirXml = req.read().decode('utf-8')
+            if SeoulAirXml == None:
+                print("에러")
+            else:
+                self.parseData = parseString(SeoulAirXml)
+                self.Update=self.parseData.getElementsByTagName('MSRDATE')
+                self.GuNameData=self.parseData.getElementsByTagName('MSRSTENAME')#구이름
+                self.GradeMaxIndex=self.parseData.getElementsByTagName('MAXINDEX')#대기상태 숫자수치
+                self.GradeData = self.parseData.getElementsByTagName('GRADE')#보통 나쁨 그거..
+                self.Pm10Data = self.parseData.getElementsByTagName('PM10')#미먼
+                self.Pm25Data = self.parseData.getElementsByTagName('PM25')#초미세
+                self.NitroData=self.parseData.getElementsByTagName('NITROGEN') #이산화질소
+                self.OzoneData = self.parseData.getElementsByTagName('OZONE')  # 오존
+                self.CarbonData = self.parseData.getElementsByTagName('CARBON')  # 일산화탄소
+                self.SurfulSData = self.parseData.getElementsByTagName('SULFUROUS')  # 아황산
+
+                if len(self.Update)>0:
+                    xmltag=self.Update[0].toxml()
+                    self.date=xmltag.replace('<MSRDATE>','').replace('</MSRDATE>','')
+
+
+        self.SetDatastoList()
+
+    def SetDatastoList(self):
+        i=0
+        for one_tag in self.GuNameData:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<MSRSTENAME>', '').replace('</MSRSTENAME>', '')
+            self.DataList[0][i]=self.xmlData
+            i+=1
+        i=0
+        for one_tag in self.GradeData:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<GRADE>', '').replace('</GRADE>', '')
+            self.DataList[1][i]=self.xmlData
+            i += 1
+        i=0
+        for one_tag in self.GradeMaxIndex:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<MAXINDEX>', '').replace('</MAXINDEX>', '')
+            self.DataList[2][i]=self.xmlData
+            i += 1
+        i=0
+        for one_tag in self.Pm10Data:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<PM10>', '').replace('</PM10>', '')
+            self.DataList[3][i]=self.xmlData
+            i += 1
+        i=0
+        for one_tag in self.Pm25Data:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<PM25>', '').replace('</PM25>', '')
+            self.DataList[4][i]=self.xmlData
+            i += 1
+        i = 0
+        for one_tag in self.NitroData:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<NITROGEN>', '').replace('</NITROGEN>', '')
+            self.DataList[5][i]=self.xmlData
+            i += 1
+
+        i = 0
+        for one_tag in self.OzoneData:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<OZONE>', '').replace('</OZONE>', '')
+            self.DataList[6][i] = self.xmlData
+            i += 1
+        i = 0
+        for one_tag in self.CarbonData:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<CARBON>', '').replace('</CARBON>', '')
+            self.DataList[7][i] = self.xmlData
+            i += 1
+        i = 0
+        for one_tag in self.SurfulSData:
+            self.xmlTag = one_tag.toxml()
+            self.xmlData = self.xmlTag.replace('<SULFUROUS>', '').replace('</SULFUROUS>', '')
+            self.DataList[8][i] = self.xmlData
+            i += 1
+        i = 0
+
+    def printData(self):
+        for i in range(25):
+            print(self.DataList[0][i])
+
+
+    def startChat(self):
+        # 구현해야하는 것 - 사용자가 아무말이나 치면 아무말에서 얻은 id 값으로 '~체크 봇입니다' 출력하기
+        self.chat_id = self.bot.getUpdates()[-1].message.chat.id
+        print('user id :', self.chat_id)
+        self.bot.sendMessage(self.chat_id, '서울 대기오염 체크봇입니다.')
+
+
+
+setData()
--- 2020-1 스크립트언어 텀프로젝트/telegramBot.py
+++ 2020-1 스크립트언어 텀프로젝트/telegramBot.py
@@ -1,47 +1,47 @@
+# encoding=utf-8
+
 import telepot
-import time
-import Tkinter
+import telegram
+import os
+import ChatBotParsing
 from telepot.loop import MessageLoop
 
-token = '1157639195:AAElOOwbzdQAhliqYjeAqieNA3F7ImUXK9w'
-bot = telepot.Bot(token)
+class talkBot:
+    def __init__(self):
+        self.token = '1157639195:AAElOOwbzdQAhliqYjeAqieNA3F7ImUXK9w'
+        self.bot = telepot.Bot(self.token)
+        self.id = 1132666012
 
-update = bot.getUpdates()
-#bot.sendMessage(chat_id = '1132666012', text="안녕하세요. 서울시 대기오염 체크 챗봇입니다.")
+        self.update = self.bot.getUpdates()
 
-class chatBot:
-def handleMain(msg):
-    msg_type, chat_type, chat_id, msg_data, msg_id = telepot.glance(msg, long=True)
-    print(msg)
-    if msg_type == 'text':
-        for i in range(25):
-            if Tkinter.self.DataList[0][i] == msg:
-                search(i)
-                #bot.sendMessage((chat_id, ))
-        bot.sendMessage(chat_id, '그런 구는 없어요.')
+        self.bot.message.loop(self.TalkMe)
 
-
-def search(num):
-    guName = Tkinter.DataList[0][num]
-    pm10 = Tkinter.DataList[3][num]
-    pm25 = Tkinter.DataList[4][num]
-    nitrogen = Tkinter.DataList[5][num]
-    ozon = Tkinter.DataList[6][num]
-    carbon = Tkinter.DataList[7][num]
-    sulfurous = Tkinter.DataList[8][num]
-
-    update.message.reply_text('실시간'+guName+'대기오염 지수\n'
-                                           '미세먼지'+pm10+'㎍/㎥\n'
-                                           '초미세먼지'+pm25+'㎍/㎥\n'
-                                           '이산화질소'+nitrogen+'ppm\n'
-                                           '오존'+ozon+'ppm\n'
-                                           '일산화탄소'+carbon+'ppm\n'
-                                           '아황산가스'+sulfurous+'ppm\n')
+        me = self.bot.getMe()
+        print(me)
 
 
 
+    def TalkMe(self, msg):
+        msg_type, chat_type, chat_id, msg_data, msg_id = telepot.glance(msg, long=True)
+        print(msg)
+        if msg_type == 'text':
+            if msg['text'] == '서울':
+                self.bot.sendMessage(chat_id, '지역구를 입력해주세요')
+                if msg_type == 'text':
+                    for i in range(25):
+                        if ChatBotParsing.self.DataList[0][i] == msg['text']:
+                            self.sendData(i)
+
+    def sendData(self, num):
+        self.message.repay_text('지역구:'+ChatBotParsing.self.DataList[0][num]+'\n'+
+                                '미세먼지'+ChatBotParsing.self.DataList[3][num]+'\n'+
+                                '초미세먼지'+ChatBotParsing.self.DataList[4][num]+'\n'+
+                                '이산화질소'+ChatBotParsing.self.DataList[5][num]+'\n'+
+                                '오존'+ChatBotParsing.self.DataList[6][num]+'\n'+
+                                '일산화탄소'+ChatBotParsing.self.DataList[7][num]+'\n'+
+                                '아황산가스'+ChatBotParsing.self.DataList[8][num])
 
 
-MessageLoop(bot, handleMain).run_as_thread()
-while True:
-    time.sleep(10)
+
+
+talkBot()
