21074f8ca6e87d330a51a186e313aaaeb3ae0c6e
daydreamwarrior <32808663+daydreamwarrior@users.noreply.github.com>
2020-06-20 12:54:54 +0900

    이메일에 첨부파일로 사진삽입했으나 생각보다 별로라 점심먹고 html 이용하여 본문 내용 안에 들어가도록 수정예정

--- 2020-1 스크립트언어 텀프로젝트/CMail.py
+++ 2020-1 스크립트언어 텀프로젝트/CMail.py
@@ -1,5 +1,8 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
 class Mail:
     def __init__(self):
@@ -10,14 +13,21 @@ class Mail:
         self.session.login('parksiyeon1020@gmail.com','meayskqrggkwsbwa')
 
     def MessageSet(self,receiver,lst,datelst,idx):
+        msg = MIMEMultipart()
+
         address=["",]
         address.append(str(receiver))
-        content="※"+str(lst[0][idx])+'의 대기 상태※\n'+'최신 업데이트: '+str(datelst[0:4])+'년 '+str(datelst[4:6])+'월 '+str(datelst[6:8])+'일 '+str(datelst[8:10])+"시 00분\n"+"\n상태: "+str(lst[1][idx])+"(수치:"+str(lst[2][idx]+")\n")+"미세먼지: "+str(lst[3][idx])+"㎍/㎥ \n"+"초미세먼지: "+str(lst[4][idx])+"㎍/㎥ \n"+"이산화질소: "+str(lst[5][idx])+"ppm\n"+"오존: "+str(lst[6][idx])+"ppm\n"+"일산화탄소: "+str(lst[7][idx])+"ppm\n"+"아황산가스: "+str(lst[8][idx])+"ppm"
-        msg = MIMEText(content)
+        content=MIMEText("※"+str(lst[0][idx])+'의 대기 상태※\n'+'최신 업데이트: '+str(datelst[0:4])+'년 '+str(datelst[4:6])+'월 '+str(datelst[6:8])+'일 '+str(datelst[8:10])+"시 00분\n"+"\n상태: "+str(lst[1][idx])+"(수치:"+str(lst[2][idx]+")\n")+"미세먼지: "+str(lst[3][idx])+"㎍/㎥ \n"+"초미세먼지: "+str(lst[4][idx])+"㎍/㎥ \n"+"이산화질소: "+str(lst[5][idx])+"ppm\n"+"오존: "+str(lst[6][idx])+"ppm\n"+"일산화탄소: "+str(lst[7][idx])+"ppm\n"+"아황산가스: "+str(lst[8][idx])+"ppm")
 
-        msg['Subject'] = '실시간 서울시 대기오염 상태:'+str(lst[0][idx])
+        imgsource=open('emailattachimg.png','rb').read()
+        img=MIMEImage(imgsource,name=os.path.basename('emailattachimg.png'))
+
+        msg['Subject'] = '실시간 서울시 대기오염 상태'
         msg['From']="parksiyeon1020@gmail.com"
         msg['To']=",".join(address)
+
+        msg.attach(content)
+        msg.attach(img)
 
         self.session.sendmail('parksiyeon1020@gmail.com',address, msg.as_string())
         self.session.quit()
